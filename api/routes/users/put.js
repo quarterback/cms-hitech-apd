a10f9188af7c62bf987ec7fd4ccde0ee720f463d
Greg Walker <michael.walker@gsa.gov>
2018-04-18 13:02:55 -0500

    update unit tests

--- api/routes/users/put.js
+++ api/routes/users/put.js
@@ -1,16 +1,21 @@
 const logger = require('../../logger')('users route post');
 const defaultUserModel = require('../../db').models.user;
 const { can, synchronizeSpecific } = require('../../middleware');
+const { cache, modelIndex } = require('../../middleware/cache');
 
-const syncResponder = (UserModel = defaultUserModel) => async req => {
-  const user = await UserModel.where({ id: req.params.id }).fetch();
-  if (!user) {
-    const error = new Error();
-    error.statusCode = 404;
-    throw error;
-  }
-  return { model: user, action: 'edit-user' };
-};
+const syncResponder = (UserModel = defaultUserModel) =>
+  cache(
+    ['user endpoint sync responder', modelIndex(UserModel)],
+    () => async req => {
+      const user = await UserModel.where({ id: req.params.id }).fetch();
+      if (!user) {
+        const error = new Error();
+        error.statusCode = 404;
+        throw error;
+      }
+      return { model: user, action: 'edit-user' };
+    }
+  );
 
 module.exports = app => {
   logger.silly('setting up PUT /users/:id route');
--- api/routes/users/put.test.js
+++ api/routes/users/put.test.js
@@ -1,7 +1,7 @@
 const tap = require('tap');
 const sinon = require('sinon');
 
-const can = require('../../middleware').can;
+const { can, synchronizeSpecific } = require('../../middleware');
 const putEndpoint = require('./put');
 
 tap.test('users PUT endpoint', async endpointTest => {
@@ -9,17 +9,12 @@ tap.test('users PUT endpoint', async endpointTest => {
   const app = {
     put: sandbox.stub()
   };
-  const User = {
-    get: sandbox.stub(),
-    set: sandbox.stub(),
-    save: sandbox.stub(),
-    toJSON: sandbox.stub(),
-    validate: sandbox.stub()
-  };
+
   const UserModel = {
-    fetch: sandbox.stub(),
-    where: sandbox.stub()
+    where: sandbox.stub(),
+    fetch: sandbox.stub()
   };
+
   const res = {
     status: sandbox.stub(),
     send: sandbox.stub(),
@@ -30,126 +25,69 @@ tap.test('users PUT endpoint', async endpointTest => {
     sandbox.resetBehavior();
     sandbox.resetHistory();
 
+    UserModel.where.returns(UserModel);
+
     res.status.returns(res);
     res.send.returns(res);
     res.end.returns(res);
-
-    UserModel.where.returns({ fetch: UserModel.fetch });
   });
 
   endpointTest.test('setup', async setupTest => {
-    putEndpoint(app, UserModel);
+    putEndpoint(app);
 
     setupTest.ok(
-      app.put.calledWith('/users/:id', can('edit-users'), sinon.match.func),
+      app.put.calledWith(
+        '/users/:id',
+        can('edit-users'),
+        synchronizeSpecific(putEndpoint.syncResponder())
+      ),
       'users PUT endpoint is registered'
     );
   });
 
-  endpointTest.test('edit users handler', async handlerTest => {
-    let handler;
-    handlerTest.beforeEach(async () => {
-      putEndpoint(app, UserModel);
-      handler = app.put.args.find(args => args[0] === '/users/:id')[2];
-    });
+  endpointTest.test(
+    'synchronize responder returns the expected data',
+    async responderTests => {
+      responderTests.test(
+        'throws an error if user is not found',
+        async test => {
+          UserModel.fetch.resolves(null);
+          const responder = putEndpoint.syncResponder(UserModel);
 
-    handlerTest.test(
-      'sends a not found error if requesting to edit a user that does not exist',
-      async notFoundTest => {
-        const req = { params: { id: 1 }, body: { name: 'person' } };
-        UserModel.fetch.resolves(null);
-
-        await handler(req, res);
-
-        notFoundTest.ok(res.status.calledWith(404), 'HTTP status set to 404');
-        notFoundTest.ok(res.send.notCalled, 'no body is sent');
-        notFoundTest.ok(res.end.calledOnce, 'response is terminated');
-      }
-    );
-
-    handlerTest.test(
-      'sends a server error if anything goes wrong',
-      async saveTest => {
-        const req = {
-          params: { id: 1 },
-          body: { name: 'bob', activities: [1, 2] }
-        };
-
-        UserModel.fetch.rejects();
-
-        await handler(req, res);
-
-        saveTest.ok(res.status.calledWith(500), 'HTTP status set to 500');
-      }
-    );
-
-    handlerTest.test(
-      'rejects if the data model validation fails',
-      async invalidTest => {
-        const req = {
-          params: { id: 1 },
-          body: { name: 'Bob', email: 'new@email.com' }
-        };
-        User.get
-          .withArgs('id')
-          .returns('bob-id')
-          .withArgs('email')
-          .returns('old@email.com');
-        User.save.resolves();
-        User.validate.rejects(new Error('invalidate-test'));
-
-        UserModel.fetch.resolves(User);
-
-        await handler(req, res);
-
-        invalidTest.ok(res.status.calledWith(400), 'HTTP status set to 400');
-        invalidTest.ok(
-          res.send.calledWith({ error: 'edit-user-invalidate-test' }),
-          'error token is set'
-        );
-        invalidTest.ok(res.end.called, 'response is terminated');
-      }
-    );
-
-    handlerTest.test('updates a valid user object', async validTest => {
-      const req = {
-        params: { id: 1 },
-        body: { name: 'Bob', email: 'new@email.com' }
-      };
-      User.get
-        .withArgs('id')
-        .returns('bob-id')
-        .withArgs('email')
-        .returns('old@email.com');
-      User.save.resolves();
-      User.toJSON.returns({ name: 'json-name' });
-      User.validate.resolves();
-
-      UserModel.fetch.resolves(User);
-
-      await handler(req, res);
-
-      validTest.ok(
-        User.set.calledWith({ name: 'Bob' }),
-        'sets the user name field'
+          try {
+            await responder({ params: { id: 'user-id' } });
+            test.fail('rejects');
+          } catch (e) {
+            test.pass('rejects');
+            test.ok(
+              UserModel.where.calledWith({ id: 'user-id' }),
+              'searches for the specified user'
+            );
+            test.equal(e.statusCode, 404, 'exception has 404 status code');
+            test.notOk(e.error, 'exception does not have an error property');
+          }
+        }
       );
-      validTest.ok(
-        User.set.calledWith({ email: 'new@email.com' }),
-        'sets the new user email'
+
+      responderTests.test(
+        'returns the user and action definition',
+        async test => {
+          UserModel.fetch.resolves('this is a user!');
+          const responder = putEndpoint.syncResponder(UserModel);
+
+          const out = await responder({ params: { id: 'user-id' } });
+
+          test.ok(
+            UserModel.where.calledWith({ id: 'user-id' }),
+            'searches for the specified user'
+          );
+          test.same(
+            out,
+            { model: 'this is a user!', action: 'edit-user' },
+            'sends back the expected model and action'
+          );
+        }
       );
-      validTest.ok(
-        User.save.calledAfter(User.set),
-        'the model is saved after values are set'
-      );
-      validTest.ok(res.status.notCalled, 'HTTP status is not explicitly set');
-      validTest.ok(
-        User.toJSON.calledOnce,
-        'database object is converted to pure object'
-      );
-      validTest.ok(
-        res.send.calledWith({ name: 'json-name' }),
-        'updated user data is sent'
-      );
-    });
-  });
+    }
+  );
 });
